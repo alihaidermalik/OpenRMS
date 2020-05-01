c3a0cb862c0fe124f8fc54a4fa620b5476785d11
Ali Haider <amalik.knysys@gmail.com>
2020-05-01 14:40:33 +0500

    Completed model and django admin panel

--- resturant/admin.py
+++ resturant/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from resturant.models import Branch
+from resturant.models import Table
+from resturant.models import TableStatus
+from resturant.models import Customer
+from resturant.models import Reservation
+from resturant.models import Order
+from resturant.models import TableTrack
+from resturant.models import Tax
+from resturant.models import DiscountType
+from resturant.models import Bill
+from resturant.models import Menu
+from resturant.models import MenuItem
+from resturant.models import Deal
+from resturant.models import DealItem
+from resturant.models import OrderDetails
+from resturant.models import Employee
+from resturant.models import DeliveryTrack
+from resturant.models import PayRoll
+from resturant.models import Revenue
+from resturant.models import Feedback
+
 
 # Register your models here.
+admin.site.register(Branch)
+admin.site.register(Table)
+admin.site.register(TableStatus)
+admin.site.register(Customer)
+admin.site.register(Reservation)
+admin.site.register(Order)
+admin.site.register(TableTrack)
+admin.site.register(Tax)
+admin.site.register(DiscountType)
+admin.site.register(Bill)
+admin.site.register(Menu)
+admin.site.register(MenuItem)
+admin.site.register(Deal)
+admin.site.register(DealItem)
+admin.site.register(OrderDetails)
+admin.site.register(Employee)
+admin.site.register(DeliveryTrack)
+admin.site.register(PayRoll)
+admin.site.register(Revenue)
+admin.site.register(Feedback)
--- resturant/models.py
+++ resturant/models.py
@@ -4,32 +4,28 @@ from django.db import models
 
 class Branch(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     address = models.CharField(max_length=200, null=True)
     active = models.BooleanField(default=True)
     phone = models.IntegerField(max_length=10, null=True)
 
 class Table(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     active = models.BooleanField(default=True)
     chairs = models.IntegerField()
     branch_id = models.ForeignKey(Branch, on_delete=models.CASCADE)
 
 class TableStatus(models.Model):
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
-    status = models.TextChoices('Free', 'Reserved', 'Busy')
-
-class TableTrack(models.Model):
-    serial = models.AutoField(primary_key=True)
-    table_id = models.ForeignKey(Table, on_delete=models.CASCADE)
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    status = (('free','Free'),('reserved','Reserved'),('busy','Busy'))
 
 class Customer(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    phone = models.IntegerField(max_length=10)
+    phone = models.IntegerField(max_length=10, unique=True)
     address = models.CharField(max_length=200)
+    password = models.CharField(max_length=10)
 
 class Reservation(models.Model):
     id = models.AutoField(primary_key=True)
@@ -38,42 +34,85 @@ class Reservation(models.Model):
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
-class OrderType(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.TextChoices('Take away', 'Dine in', 'Home delivery')
-
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
     date = models.DateTimeField(auto_now_add=True, blank=True)
-    orderType_id = models.ForeignKey(OrderType, on_delete=models.SET_NULL)
+    order_choices = (('takeAway','Take away'), ('dineIn','Dine in'), ('homeDelivery','Home delivery'))
+    order_type = models.CharField(choices=order_choices,max_length=200)
 
-class OrderDetails(models.Model):
+class TableTrack(models.Model):
     serial = models.AutoField(primary_key=True)
-    item_id = models.ForeignKey(MenuItem, on_delete=models.SET_NULL)
-    quantity = models.IntegerField()
+    table_id = models.ForeignKey(Table, on_delete=models.CASCADE)
     order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
-
-class Bill(models.Model):
-    id = models.AutoField(primary_key=True)
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True, blank=True)
-    amount = models.IntegerField()
-    discount_id = models.ForeignKey(DiscountType, on_delete=models.SET_NULL)
-    tax_id = models.ForeignKey(Tax, on_delete=models.CASCADE)
 
 class Tax(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     amount = models.IntegerField(max_length=2)
     active = models.BooleanField(default=True)
-    orderType_id = models.ForeignKey(OrderType, on_delete=models.SET_NULL)
+    order_choices = (('takeAway','Take away'), ('dineIn','Dine in'), ('homeDelivery','Home delivery'))
+    order_type = models.CharField(choices=order_choices,max_length=200)
 
 class DiscountType(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     amount = models.IntegerField(max_length=2)
     active = models.BooleanField(default=True)
-    orderType_id = models.ForeignKey(OrderType, on_delete=models.SET_NULL)
+    order_choices = (('takeAway','Take away'), ('dineIn','Dine in'), ('homeDelivery','Home delivery'))
+    order_type = models.CharField(choices=order_choices,max_length=200)
+
+class Bill(models.Model):
+    id = models.AutoField(primary_key=True)
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+    amount = models.IntegerField()
+    discount_id = models.ForeignKey(DiscountType, null=True, blank=True, on_delete=models.SET_NULL)
+    tax_id = models.ForeignKey(Tax, on_delete=models.CASCADE)
+
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(unique=True, upload_to = 'pictures')
+
+class MenuItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    price = models.IntegerField()
+    discount = models.IntegerField(max_length=2)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(unique=True, upload_to = 'pictures')
+
+class Deal(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+    price = models.IntegerField()
+    active = models.BooleanField(default=True)
+    image = models.ImageField(unique=True, upload_to = 'pictures')
+
+class DealItem(models.Model):
+    deal_id = models.ForeignKey(Deal, on_delete=models.CASCADE)
+    item_id = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+
+class OrderDetails(models.Model):
+    serial = models.AutoField(primary_key=True)
+    item_id = models.ForeignKey(MenuItem, null=True, blank=True, on_delete=models.SET_NULL)
+    quantity = models.IntegerField()
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    
+class Employee(models.Model):
+    id = models.AutoField(primary_key=True)
+    branch_id = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    phone = models.IntegerField(max_length=10)
+    address = models.CharField(max_length=200)
+    role_choice = (('admin','Admin'), ('superAdmin','Super Admin'), ('manager','Manager'), ('deliveryBoy','Delivery Boy'), ('waiter','Waiter'), ('cashier','Cashier'))
+    role = models.CharField(choices=role_choice,max_length=200)
+    salary = models.IntegerField()
+    password = models.CharField(max_length=10)
+    active = models.BooleanField(default=True)
 
 class DeliveryTrack(models.Model):
     serial = models.AutoField(primary_key=True)
@@ -83,34 +122,10 @@ class DeliveryTrack(models.Model):
     employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
     bill_id = models.ForeignKey(Bill, on_delete=models.CASCADE)
 
-class Menu(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
-
-class MenuItem(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.IntegerField()
-    discount = models.IntegerField(max_length=2)
- 
- class Employee(models.Model):
-    id = models.AutoField(primary_key=True)
-    branch_id = models.ForeignKey(Branch, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    phone = models.IntegerField(max_length=10)
-    address = models.CharField(max_length=200)
-    role_id = models.ForeignKey(Role, on_delete=models.CASCADE)
-    salary = models.IntegerField()
-
 class PayRoll(models.Model):
     employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
     date = models.DateTimeField()
     bonus = models.IntegerField()
-
-class Role(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.TextChoices('Admin', 'Super Admin', 'Manager', 'Delivery Boy', 'Waiter', 'Cashier')
 
 class Revenue(models.Model):
     date = models.DateTimeField(auto_now_add=True, blank=True)
